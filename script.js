a4349eba2d0f72c9a85d85753fadb2c12c663187
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-09-03 16:18:06 +0300

    correction bug

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import responsiveCanvas from "./functions/responsiveCanvas.js";
-import drawGame from "./functions/drawGame.js";
 import responsiveJoystick from "./functions/responsiveJoystick.js";
 import animationOfResponsiveJoystick from "./functions/animationOfResponsiveJoystick.js";
-import drawTurgut from "./functions/drawGame.js";
+import drawGame from "./functions/drawGame.js";
 
 const prohibitedColors = ["#3d34a5", "#a8a8a8", "#7b7b7b", "#139d08", "#c5324f", "#000000", "#823c3d", "#aE6c37"];
 const canvas = document.querySelector("#canvas");
@@ -20,7 +19,7 @@ function draw() {
     responsiveJoystick();
     // Fonction qui ont besoin d'être redessinées
     setTimeout(function () {
-        drawTurgut();
+        drawGame();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         animationOfResponsiveJoystick();
     }, 1000 / fps);
